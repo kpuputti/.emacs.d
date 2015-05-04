fbcd824f0f10030c1382a39f759bc2cf5f7e0392
Kimmo Puputti <kpuputti@gmail.com>
2015-05-04 23:09:57 +0300

    Add helper macro to rename major mode modeline.

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -1,4 +1,10 @@
 ;;; init-utils.el
 
+;; http://whattheemacsd.com/appearance.el-01.html
+(defmacro rename-modeline (package-name mode new-name)
+  `(eval-after-load ,package-name
+     '(defadvice ,mode (after rename-modeline activate)
+        (setq mode-name ,new-name))))
+
 (provide 'init-utils)
 ;;; init-utils.el ends here
